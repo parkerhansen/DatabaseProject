62ae242f262a9d18bfd741943bbd6260f82192be
parkerhansen <40070117+parkerhansen@users.noreply.github.com>
2018-11-25 12:45:01 -0600

    Changing Links

--- ProjectHTML.php
+++ ProjectHTML.php
@@ -1,15 +1,67 @@
 <html>
+<head>
+<style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+th, td {
+    padding: 15px;
+}
+</style>
+</head>
 <body>
-<h1>Group 10 Internet of Things Database</h1>
-<form action="Connector.php" method="post">
 
-ProviderName: <input type="text" name="ProviderName" /><br><br>
+<h1><center>A DATABASE FOR YOUR INTERNET OF THINGS:</center></h1>
+<center><img src="http://localhost/ElitaDrawing.jpeg" /></center>
+<h2><center>HELPING YOU SEE BOTH THE FOREST AND THE TREES</center></h2>
 
-PhoneNumber: <input type="text" name="PhoneNumber" /><br><br>
+<table>
+<tr>
+<td>
+
+<form action="Query.php" method="post">
+
+<center>Insert a new Provider</center><br>
+
+Provider Name: <input type="text" name="ProviderName" /><br><br>
+
+Phone Number: <input type="text" name="PhoneNumber" /><br><br>
 
 <input type="submit" />
 
 </form>
+
+</td>
+<td>
+
+<form action="Query.php" method="post">
+
+<center>Insert a new user</center><br>
+
+User SSN: <input type="text" name="SSN" /><br><br>
+
+First Name: <input type="text" name="Fname" /><br><br>
+
+Last Name: <input type="text" name="Lname" /><br><br>
+
+Gender:
+<input type="radio" name="Sex" value="F">Female
+<input type="radio" name="Sex" value="M">Male
+<input type="radio" name="Sex" value="O">Other <br><br>
+
+Authorized User?:
+<input type="radio" name="AuthUser" value="Yes">Yes
+<input type="radio" name="AuthUser" value="No">No <br><br>
+
+<input type="submit" />
+
+</form>
+
+</td>
+</tr>
+</table>
+
 
 </body>
 </html>
