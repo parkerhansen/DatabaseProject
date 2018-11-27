335d63f1f4b762c51db2d2a6c5578e012a383f34
parkerhansen <40070117+parkerhansen@users.noreply.github.com>
2018-11-27 16:05:06 -0600

    Update Query.php

--- Query.php
+++ Query.php
@@ -1,15 +1,24 @@
 <html>
+<head>
+  <link href="https://fonts.googleapis.com/css?family=Sedgwick+Ave" rel="stylesheet">
+  <style>
+    body{
+      font-family: 'Sedgwick Ave', cursive;
+    }
+  </style>
+</head>
 <body>
 <h1><center>A DATABASE FOR YOUR INTERNET OF THINGS:</center></h1>
 <center><img src="http://localhost/ElitaDrawing.jpeg" /></center>
 <h2><center>HELPING YOU SEE BOTH THE FOREST AND THE TREES</center></h2>
+
 <?php
 
 require 'Connector.php';
 
 mysqli_select_db($conn, "Project");
 
-if (!empty($_POST["ProviderName"])) {
+if (isset($_POST['SubmitProvider']) && !empty($_POST['ProviderName'])) {
 
   $sql="INSERT INTO `Provider` (`ProviderName`, `PhoneNumber`) VALUES
     ('$_POST[ProviderName]','$_POST[PhoneNumber]')";
@@ -21,50 +30,6 @@ if (!empty($_POST["ProviderName"])) {
   else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
-  mysqli_close($conn);
-}
-
-else if (!empty($_POST["SSN"])) {
-
-  if($_POST['AuthUser'] == "Yes") {
-    echo "<form method="post" action="$_SERVER["PHP_SELF"]">
-            Customer ID: <input type="text" name="CustID" /><br><br>"
-  }
-
-  $sql="INSERT INTO `Users` (`Fname`, `Lname`, `SSN`, `Sex`) VALUES
-    ('$_POST[Fname]', '$_POST[Lname]', '$_POST[SSN]', '$_POST[Sex]')";
-
-  if (mysqli_query($conn, $sql)) {
-    echo "New User record created successfully";
-  }
-  else {
-    echo "Error:" . $sql . "<br>" . mysqli_error($conn);
-  }
-
-  if($_POST['AuthUser'] == "Yes") {
-    $sql="INSERT INTO `AuthorizedUser` (`SSN`, `CustomerID`) VALUES
-      ('$_POST[SSN]', '$_POST[CustID]')";
-
-      if (mysqli_query($conn, $sql)) {
-        echo "<br><br>Authorized User record created successfully";
-      }
-      else {
-        echo "Error:" . $sql . "<br>" . mysqli_error($conn);
-      }
-  }
-
-  else if ($_POST['AuthUser'] == "No") {
-    $sql="INSERT INTO `SecondaryUser` (`SSN`) VALUES
-      ('$_POST[SSN]')";
-
-    if (mysqli_query($conn, $sql)) {
-      echo "<br><br>Secondary User record created successfully";
-    }
-    else {
-      echo "Error:" . $sql . "<br>" . mysqli_error($conn);
-    }
-  }
-
   mysqli_close($conn);
 }
 
